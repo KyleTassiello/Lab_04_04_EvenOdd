87e5e36b06ccea4dc6332e5e6e6836abe97b0e3c
KyleTassiello <tassiekd@mail.uc.edu>
2024-09-19 14:14:42 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        int initialValue = 2;
+        System.out.println("Your value is " + initialValue);
+        if (initialValue % 2 == 1)
+        {
+            System.out.println("Your value is odd");
+        }
+        else
+        {
+            System.out.println("Your value is even");
+        }
+
+    }
+}
